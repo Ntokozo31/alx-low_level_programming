f7f6c587e6c800644e73079a45d72d589bffe906
Ntokozo31 <dubentokozo45@yahoo.com>
2023-09-13 17:06:58 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+void prints_times_table(int n)
+{
+if (n < 0 || n > 15)
+{
+	return;
+}
+for (i = 0; i <= 10; i++)
+{
+printf("%2d x %2d = %2d\n", i, n, i, *n);
+}
+}
+
+int main(void)
+{
+int n;
+printf("Enter a number between 0 and 15:");
+scanf("%d", &n);
+prints_times_table(n);
+return (0);
+}
