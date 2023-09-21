059a2f7399689f2d232b3c53740a93c937d1a2f9
Ntokozo31 <dubentokozo45@yahoo.com>
2023-09-21 07:53:34 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
 char *_strcat(char *dest, char *src);
 
 char *_strncat(char *dest, char *src, int n);
@@ -9,4 +14,4 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 
 char *string_toupper(char *);
-
+#endif
