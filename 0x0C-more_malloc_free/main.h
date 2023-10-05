8ae2dc1418412b2e7dc762395f40e509cf8f74a0
Ntokozo31 <dubentokozo45@yahoo.com>
2023-10-05 13:25:22 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,6 +9,10 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 
 int *array_range(int min, int max);
 
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+
+
 #include <stdio.h>
 
 #include <stdlib.h>
