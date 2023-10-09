3c46f7acafa1a873fc2d7a8a558798b034f35a03
Ntokozo31 <dubentokozo45@yahoo.com>
2023-10-09 13:10:40 -0700

    5-free_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,20 +1,38 @@
 #include "dog.h"
 
-/*
- * int_dog - initialize a structure of type dog
- * @d: pointer to structure
- * @name: pointer to the name of the dog
- * @age: age of the dog
- * @owner: pointer to owner of dog
- * Return: void
- */
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
+/**
+
+* init_dog - initializes a structure of type dog
+
+* @d: pointer to structure
+
+* @name: pointer to the name of the dog
+
+* @age: age of dog
+
+* @owner: pointer to owner of dog
+
+*
+
+* Return: void
+
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+
 {
-	if (d)
-	{
-		d - >name = name;
-		d - >age = age;
-		d - >owner = owner;
-	}
+
+   if (d)
+
+   {
+
+       d->name = name;
+
+       d->age = age;
+
+       d->owner = owner;
+
+   }
+
 }
