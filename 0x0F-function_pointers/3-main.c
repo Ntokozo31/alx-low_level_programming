c8538a82911a88ecddbfd4464a490a24505b1dcd
Ntokozo31 <dubentokozo45@yahoo.com>
2023-10-11 12:10:19 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,11 +13,12 @@
 int main(int argc, char *argv[])
 {
 	int (*oprt)(int, int);
+
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
 	}
-	printf("%d\n", oprt(atoi(argv[1]),atoi(argv[3])));
+	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
