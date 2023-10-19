7b39bd413bf76ba49c64ff272d80970c4f354d9e
Ntokozo31 <dubentokozo45@yahoo.com>
2023-10-19 16:05:44 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * bmain - function executed before main
+ * ntokozomain - function executed before main
  * Return: no return
  */
 
-void __attribute__ ((constructor)) bmain()
+void __attribute__ ((constructor)) ntokozomain()
 
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!");
+	printf("I bore my house upon my back!\n");
 }
